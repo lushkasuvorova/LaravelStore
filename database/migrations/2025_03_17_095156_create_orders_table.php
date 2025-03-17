de821d7f0ab10daca6196bfaf48db41d1667cd38
lushkasuvorova <lushkasuvorova@gmail.com>
2025-03-17 13:30:09 +0300

    migrate

---
+++ database/migrations/2025_03_17_095156_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['новый', 'выполнен'])->default('новый');
+            $table->string('number')->unique();
+            $table->string('customer_fio');
+            $table->text('customer_komment')->nullable();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade'); // Связь с товаром
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
